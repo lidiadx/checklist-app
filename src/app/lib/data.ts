5dc2ebca975f88ece427ee1900767f67930e71b2
Lidia D <lidia.khmylko@gmail.com>
2024-02-27 10:06:05 +0100

    clean fetching data: use findOne mongodb function

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,19 +1,34 @@
 import clientPromise from "@/app/lib/mongodb";
 
+const db_name = "checklist-db-demo";
+const collection = "users";
+
 export async function fetchUserData() {
   try {
       const client = await clientPromise;
-      const db = client.db("checklist-db-demo");
+      const db = client.db(db_name);
 
       const d = await db
-          .collection("users")
-          .find({name: 'testuser'})
-          .toArray();
-        
-      const data = d[0].data;  // expect only one result
+        .collection(collection)
+        .findOne({name: 'testuser'});
+
+      const data = d?.data;
 
       return data;
   } catch (e) {
       console.error(e);
   }
+}
+
+// TODO: test this function
+export async function updateUserData(userData: any) { 
+    try {
+        const client = await clientPromise;
+        const db = client.db(db_name);
+        const d = await db
+        .collection(collection)
+        .updateOne({name: 'testuser'}, userData);
+    } catch(e) {
+        console.error(e);
+    }
 }
